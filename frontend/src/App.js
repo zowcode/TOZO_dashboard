8c4d2c4194f347796b650da177324584f5030393
Zow <zcompoint@gmail.com>
2019-11-21 23:35:45 +0100

    rentrer en dur données widget dans dashboard + affichage des ces données dans widgetadd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from "react";
 import './App.css';
 import Dashboard from './component/Dashboard.js';
 import APIpage from './component/APIpage.js';
 import ProfilBar from './component/ProfilBar.js';
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      profil: 1
+      
+    };
+  }
+
+  render() {
+    return (
       <div className="row principal " >
 
         <div className = " profilBar col col-2 col-lg-2 col-md-2 col-sm-2">
@@ -17,7 +27,8 @@ function App() {
        </div>
 
       </div>
-  );
+    );
+  }
 }
 
 export default App;
--- frontend/src/component/Dashboard.js
+++ frontend/src/component/Dashboard.js
@@ -1,41 +1,116 @@
 import React from 'react';
+import { Component } from "react";
 import '../App.css';
 import Widget from './Widget.js';
 
-function Dashboard() {
-  return (
-    <div>
-        <div className="row pageName">
-            <h1> DASHBOARD </h1>
-        </div>
+class Dashboard extends Component {
 
-        <div className="row widgetContainer">
+    // CONSTRUCTEUR
+    constructor(props) {
 
-            <div className="col">
-                <Widget></Widget>
-            </div>
-            <div className="col">
-                <Widget></Widget>
-            </div>
-            <div className="col">
-                <Widget></Widget>
-            </div>
+        super(props);
+        // STATE: données internes du composant
+        this.state = {
+      
+            widget1 : 
+            {
+               nom: "widget1",
+               type: "1",
+               content: "blablablaabalabl"
+            },
+        
+            widget2 : 
+            {
+                nom: "widget2",
+                type: "2",
+                content: "blablablaabalabl"
+            },
+
+            widget3 : 
+            {
+                nom: "widget3",
+                type: "3",
+                content: "blablablaabalabl"
+            },
+
+            widget4: 
+            {
+                nom: "widget4",
+                type: "4",
+                content: "blablablaabalabl"
+            },
+
+            widget5 : 
+            {
+                nom: "widget5",
+                type: "5",
+                content: "blablablaabalabl"
+            },
+            widget6 : 
+            {
+                nom: "widget6",
+                type: "6",
+                content: "blablablaabalabl"
+            }
+        };
+    
+    }
+
+
+    // CALL API
+    render() {
+        return (
+            <div>
+                <div className="row pageName">
+                    <h1> DASHBOARD </h1>
+                </div>
+
+                <div className="row widgetContainer">
+
+                    <div className="col">
+                        <Widget nom={this.state.widget1.nom}
+                        type={this.state.widget1.type}
+                        content={this.state.widget1.content} >  
+                        </Widget>
+                    </div>
+                    <div className="col">
+                        <Widget nom={this.state.widget2.nom}
+                        type={this.state.widget2.type}
+                        content={this.state.widget2.content} >  
+                        </Widget>
+                    </div>
+                    <div className="col">
+                        <Widget nom={this.state.widget3.nom}
+                        type={this.state.widget3.type}
+                        content={this.state.widget3.content} >  
+                        </Widget>
+                    </div>
+
+                    </div>
+                <div className="row widgetContainer">
+                    <div className="col">
+                        <Widget nom={this.state.widget4.nom}
+                        type={this.state.widget4.type}
+                        content={this.state.widget4.content} >  
+                        </Widget>
+                    </div>
+                    <div className="col">
+                        <Widget nom={this.state.widget5.nom}
+                        type={this.state.widget5.type}
+                        content={this.state.widget5.content} >  
+                        </Widget>
+                    </div>
+                    <div className="col">
+                        <Widget nom={this.state.widget6.nom}
+                        type={this.state.widget6.type}
+                        content={this.state.widget6.content} >  
+                        </Widget>
+                    </div>
+                </div>
 
             </div>
-        <div className="row widgetContainer">
-            <div className="col">
-                <Widget></Widget>
-            </div>
-            <div className="col">
-                <Widget></Widget>
-            </div>
-            <div className="col">
-                <Widget></Widget>
-            </div>
-        </div>
-
-    </div>
-  );
+        );
+    }
 }
 
 export default Dashboard;
